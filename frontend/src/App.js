0a290703a30e4bc7b277b55a2557336598a056e6
Sahils2834 <113817823+Sahils2834@users.noreply.github.com>
2025-11-29 12:47:53 +0530

    basic react file was created with assets folder

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Radhee Studio</h1>
+      <p>This is a basic React setup.</p>
+    </div>
+  );
+}
+
+export default App;
